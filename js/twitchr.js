3a29ce31fd6092ab43856d1592d6ec34a5ccebd5
platypusrex <frankcooke79@gmail.com>
2015-10-20 11:47:29 -0400

    added ngRoute, simple navigation, jade templates, and style changes to accomodate navigation

--- js/twitchr.js
+++ js/twitchr.js
@@ -1,5 +1,17 @@
 (function(){
-    angular.module('TwitchApp', []);
+    angular.module('TwitchApp', ['ngRoute'])
+        .config(['$routeProvider', function($routeProvider){
+            $routeProvider
+                .when('/featured', {
+                    templateUrl: 'featured.html',
+                    controller: 'twitchController'
+                })
+                .when('/codecamp', {
+                    templateUrl: 'codecamp.html',
+                    controller: 'campController'
+                })
+                .otherwise({redirectTo: '/featured'});
+        }]);
 }());
 
 
