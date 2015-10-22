591763cf046a39622a945a31ec5a7e4d41b1b32a
platypusrex <frankcooke79@gmail.com>
2015-10-21 22:25:20 -0400

    added $http service for offline users, the getCampStreams function for on and offline accounts, made adjustments to codecamp.jade to get data on page

--- js/campCtrl.js
+++ js/campCtrl.js
@@ -1,20 +1,66 @@
 (function(twitchapp){
-    twitchapp.controller('campController', ['$scope', 'twitchService', function($scope, twitchService){
+    twitchapp.controller('campController', ['$scope', 'twitchService', '$q', '$http', '$rootScope', function($scope, twitchService, $q, $http, $rootScope){
 
         var getCampStreams = function(){
-            var campStreams = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
-            campStreams.forEach();
-        };
+            var campStreams = ["freecodecamp", "Mr4thDimention", "MartinCohen", "abnercoimbre","dasmehdi","lsrpnet","noobs2ninjas","assertchris", "NashCasts"];
+            var promises = [];
 
-        var onSuccess = function(data){
-            $scope.codeCamp = data;
-            console.log($scope.codeCamp);
+            angular.forEach(campStreams, function(val){
+                promises.push(twitchService.getCodeCamp(val));
+            });
+            $q.all(promises).then(function(data){
+                var online = [];
+                var offline = [];
+                var channels = [];
+                angular.forEach(data, function(val){
+                    if(val.stream === null){
+                        offline.push(val);
+                    }else {
+                        online.push(val);
+                    }
+                });
+                angular.forEach(offline, function(val){
+                    channels.push(twitchService.getCodeChannel(val._links.channel));
+                });
+                $q.all(channels).then(function(data){
+                    var streamData = data;
+                    online.forEach(function(val){
+                        streamData.unshift(val);
+                    });
+
+                    $scope.streams = streamData.map(function(val){
+                        if(!val.stream){
+                            return {
+                                logo: val.logo,
+                                displayName: val.display_name,
+                                icon: 'fa fa-exclamation-triangle fa-2x',
+                                url: val.url
+                            }
+                        }else{
+                            return {
+                                logo: val.stream.channel.logo,
+                                displayName: val.stream.channel.display_name,
+                                icon: 'fa fa-check-circle-o fa-2x',
+                                followers: val.stream.channel.followers,
+                                game: val.stream.channel.game,
+                                url: val.stream.channel.url,
+                                views: val.stream.channel.views,
+                                viewers: val.stream.viewers,
+                                preview: val.stream.preview.medium,
+                                title: val.stream.channel.status
+                            }
+                        }
+                    });
+                    console.log(streamData);
+                    console.log($scope.streams);
+                });
+            }, onError);
         };
 
         var onError = function(err){
             $scope.err = 'There was a problem loading the data, dude. Try again in a bit.';
         };
 
-        twitchService.getCodeCamp().then(onSuccess, onError);
+        getCampStreams();
     }]);
 }(angular.module('TwitchApp')));
--- js/twitchrService.js
+++ js/twitchrService.js
@@ -2,18 +2,25 @@
     twitchapp.factory('twitchService', ['$http', function($http){
 
         var getStreams = function(){
-            return $http.get('https://api.twitch.tv/kraken/streams/featured')
+            return $http.jsonp('https://api.twitch.tv/kraken/streams/featured?callback=JSON_CALLBACK')
                 .then(function(res){
                     return res.data;
                 });
         };
 
         var getCodeCamp = function(username){
-            return $http.jsonp('https://api.twitch.tv/kraken/streams?callback=JSON_CALLBACK&channel=manvsgame,freecodecamp')
+            return $http.jsonp('https://api.twitch.tv/kraken/streams/' + username + '?callback=JSON_CALLBACK&')
                 .then(function(res){
                     return res.data;
                 });
         };
+
+        var getCodeChannel = function(url){
+            return $http.jsonp(url + '?callback=JSON_CALLBACK')
+                .then(function(res){
+                    return res.data;
+                });
+        }
 
         var searchStreams = function(streamName){
             return $http.jsonp('https://api.twitch.tv/kraken/streams?callback=JSON_CALLBACK&channel=' + streamName)
@@ -25,6 +32,7 @@
         return {
             getStreams: getStreams,
             getCodeCamp: getCodeCamp,
+            getCodeChannel: getCodeChannel,
             searchStreams: searchStreams
         }
 
